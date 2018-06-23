103dbf8777f622d44bc349199809e1e5c7d366e0
Kirk McCulloch <kirkdmcm@gmail.com>
2018-06-23 13:22:54 -0600

    test console print from Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,20 @@
 package com.company;
 
 import com.company.FunctionSequence.ConkSequence;
+import com.company.PureAction.ConsolePrintAction;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        testSequence();
+        testActions();
+    }
 
+    private static void testActions() {
+        new ConsolePrintAction().message("print me!").invoke();
+    }
+
+    private static void testSequence() {
         ConkSequence conkSequence = new ConkSequence();
 
         conkSequence.run();
