57bccede6a16d80cd249abadff43896ad6aa8257
Kirk <kirkdmcm@gmail.com>
2018-06-26 19:18:00 -0600

    bump particle count to 3M

--- src/com/company/BasicDOD/BasicDODDemo.java
+++ src/com/company/BasicDOD/BasicDODDemo.java
@@ -2,7 +2,7 @@ package com.company.BasicDOD;
 
 public class BasicDODDemo {
 
-    private static final int count = 2 * 1000000;
+    private static final int count = 3 * 1000000;
 
     public static void main(String[] args) {
         new Simulator(count).run();
--- src/com/company/BasicOOP/BasicOOPDemo.java
+++ src/com/company/BasicOOP/BasicOOPDemo.java
@@ -2,7 +2,7 @@ package com.company.BasicOOP;
 
 public class BasicOOPDemo {
 
-    private static final int count = 2 * 1000000;
+    private static final int count = 3 * 1000000;
 
     public static void main(String[] args) {
         new Simulator(count).run();
